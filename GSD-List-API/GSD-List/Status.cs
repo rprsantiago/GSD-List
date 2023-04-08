e623b755a8b78e1c85429844dfe1477290fd43c6
rprsantiago <125931617+rprsantiago@users.noreply.github.com>
2023-04-08 10:13:59 +0800

    Initial commit

---
+++ GSD-List-API/GSD-List/Status.cs
@@ -0,0 +1,8 @@
+﻿namespace GSD_List
+{
+    public class Status
+    {
+        public int Id { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ GSD-List-API/GSD-List/Task.cs
@@ -0,0 +1,10 @@
+﻿namespace GSD_List
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string TaskName { get; set; } = string.Empty;
+        public string TaskDescription { get; set; } = string.Empty;
+        public int StatusId { get; set; }
+    }
+}
