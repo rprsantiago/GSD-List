9047b382f8d71e804a725bb5079a6023d88d145a
rprsantiago <125931617+rprsantiago@users.noreply.github.com>
2023-04-08 10:15:12 +0800

    Added UI files to repo

---
+++ GSD-List-UI/src/app/models/codetable.ts
@@ -0,0 +1,5 @@
+export class codetable{
+    id?: number;
+    description!: string;
+    active!: boolean;
+}
---
+++ GSD-List-UI/src/app/models/task.ts
@@ -0,0 +1,12 @@
+import { codetable } from "./codetable";
+
+export class task{
+    id?: number;
+    taskName!: string;
+    taskDescription!: string;
+    //test
+
+    //relate statusId to codetable
+    statusId!: number;
+    status!: codetable;
+}
