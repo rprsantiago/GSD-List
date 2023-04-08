d62f969d75984f1d15cc1cb31a5762d7333d36a1
rprsantiago <125931617+rprsantiago@users.noreply.github.com>
2023-04-08 11:23:20 +0800

    Updated directories

--- GSD-List-API/GSD-List/Controllers/TaskController.cs
+++ GSD-List-API/GSD-List/Controllers/TaskController.cs
@@ -7,5 +7,13 @@ namespace GSD_List.Controllers
     [ApiController]
     public class TaskController : ControllerBase
     {
+        [HttpGet]
+        public async Task<ActionResult<List<Task>>> GetListTasks()
+        {
+            return new List<Task>
+            {
+                new Task { Id = 1, TaskName = "Task 1", TaskDescription = "Description 1" }
+            };
+        }
     }
 }
