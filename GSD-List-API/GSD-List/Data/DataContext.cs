0a33eb7fd5c12588f6c93f3e8c695ce090849329
rprsantiago <ronnel.santiago.gsit@pcu.edu.ph>
2023-04-10 21:19:33 +0800

    Updated namespace for Task model

--- GSD-List-API/GSD-List/Data/DataContext.cs
+++ GSD-List-API/GSD-List/Data/DataContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using GSD_List.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GSD_List.Data
 {
     public class DataContext : DbContext
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
-        public DbSet<Task> Tasks => Set<Task>();
+        public DbSet<Models.Task> Tasks => Set<Models.Task>();
     }
 }
