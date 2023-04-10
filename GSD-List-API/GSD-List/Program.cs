e85294a983907aec1b0f8728b7e9b20840240e24
rprsantiago <ronnel.santiago.gsit@pcu.edu.ph>
2023-04-10 18:56:04 +0800

    Added dbContext to Program.cs
    Added connection of EF Core to Database

--- GSD-List-API/GSD-List/Program.cs
+++ GSD-List-API/GSD-List/Program.cs
@@ -1,3 +1,6 @@
+using GSD_List.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//add dbcontext to connect to database and entity framework
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 //enable cors to access API from UI
 builder.Services.AddCors(options => options.AddPolicy(name: "GSDListOrigin",
