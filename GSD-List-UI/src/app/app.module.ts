ac5f5387f5ad86522a9efe9f59dceb2fe2e04a20
rprsantiago <ronnel.santiago.gsit@pcu.edu.ph>
2023-04-11 19:04:32 +0800

    Added Angular Material Animations, and Table to app.module.ts

--- GSD-List-UI/src/app/app.module.ts
+++ GSD-List-UI/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
