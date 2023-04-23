d8d1ae759e7b7be7df0e3fa7cfb4f8a377691d3d
rprsantiago <125931617+rprsantiago@users.noreply.github.com>
2023-04-23 12:38:57 +0800

    Added highchart for tasks

--- GSD-List-UI/src/app/components/dashboard/dashboard.component.ts
+++ GSD-List-UI/src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
+import { HighchartsChartModule } from 'highcharts-angular';
+import { codetable } from 'src/app/models/codetable';
+import { Task } from 'src/app/models/task';
+import { CodetableService } from 'src/app/services/codetable.service';
+import { TaskService } from 'src/app/services/task.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +13,50 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  tasks: Task[] = [];
+  statuses: codetable[] = [];
+
+  listStatus: any = [];
+
+  constructor(private taskService: TaskService,
+    private codeTableService: CodetableService) { }
+
+  ngOnInit(): void {
+    this.Initialize();
+  }
+
+  Initialize(){
+    //get tasks from the api
+    this.taskService
+      .getTasks()
+      .subscribe((result: Task[]) => (this.tasks = result));
+
+    this.codeTableService
+      .getStatuses()
+      .subscribe((result: codetable[]) => this.statuses = result);
+  }
+
+  Highcharts = Highcharts;
+
+  column: any = {
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Number of Tasks per Status'
+    },
+    xAxis: {
+      categories: ['test', 'test', 'test', 'test'],
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: 'Number of Tasks'
+      }
+    },
+    series: [{
+      name: 'Tasks',
+      data: [5, 3, 4, 7]
+    }]
+  }
 }
